0a7ed8d6f925f9faf88d4b149be6cc9cf4d1a940
Darshan1606 <darshantarsariya17@gmail.com>
2023-08-17 08:36:40 +0530

    connect db

--- server/routes/workouts.js
+++ server/routes/workouts.js
@@ -9,7 +9,22 @@ router.get("/", (req, res) => {
 
 //get single workout
 router.get("/:id", (req, res) => {
-  res.json({ message: "Get All Workouts" });
+  res.json({ message: "Get Single Workouts" });
+});
+
+//add workout
+router.post("/", (req, res) => {
+  res.json({ message: "ADD Workouts" });
+});
+
+//delete workout
+router.delete("/:id", (req, res) => {
+  res.json({ message: "DELETE Workouts" });
+});
+
+//update workout
+router.put("/:id", (req, res) => {
+  res.json({ message: "UPDATE Workouts" });
 });
 
 module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,30 @@
 const express = require("express");
+require("dotenv").config();
+
+const mongoose = require("mongoose");
 const wokrouts = require("./routes/workouts");
 
 // express app
 const app = express();
 
+// middleware
+app.use(express.json());
+app.use((req, res, next) => {
+  console.log(`${req.method} - ${req.url} - ${req.path}`);
+  next();
+});
+
 // routes
 app.use("/api/workouts", wokrouts);
 
-// listen for requests
-app.listen(process.env.PORT, () => {
-  console.log("listening on PORT ", process.env.PORT);
-});
+// connect to mongodb & listen for requests
+const dbURI = process.env.MONGO_URL;
+mongoose
+  .connect(dbURI)
+  .then(() => {
+    // listen for requests
+    app.listen(process.env.PORT, () => {
+      console.log("DB Connnected... & listening on PORT ", process.env.PORT);
+    });
+  })
+  .catch((err) => console.log(err));
